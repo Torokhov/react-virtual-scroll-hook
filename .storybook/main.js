264dfdcb7da8d548a4d7ecb75648ae7444ab7b49
oleg_torokhov <torokhov093@gmail.com>
2022-03-24 13:33:05 +0700

    reinit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,19 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
-    check: true, // type-check stories during Storybook build
-  }
+    check: true,
+    checkOptions: {
+      tsconfig: path.join(__dirname, '../tsconfig.json'),
+    },
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+  ],
+  framework: '@storybook/react',
 };
